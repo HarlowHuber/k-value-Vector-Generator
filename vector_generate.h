1bf85121c0810651bae9bf40e83f6a72db23364f
Harlow Huber <79harlowhuber96@gmail.com>
2022-06-04 22:02:52 -0700

    Update vector_generate.h

--- vector_generate.h
+++ vector_generate.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <vector>
+#include <map>
 #include <iostream>
 
 class vector_generate
@@ -11,36 +12,21 @@ private:
 	// number of attributes
 	int num_attributes;
 
-	// caluclate all vectors below the given max_vector
-	void calculate_all_vectors(std::vector<int> max_vector, int max_vector_index, std::vector<std::vector<int>>& all_vectors);
+	// calculate all vectors grouped by Hamming norm and sorted
+	void calculate_all_vectors(std::vector<int> max_vector, int max_hamming_norm, int max_vector_index, std::vector<std::map<int, std::vector<int>, std::greater<int>>>& sorted_vectors);
 
-	// group vectors by hamming norm
-	void group_by_hamming_norm(std::vector<std::vector<std::vector<int>>>& sorted_vectors, std::vector<std::vector<int>> unsorted_vectors);
-
-	// heap sort performed for each Hamming norm
-	void sort_vectors_in_hamming_norm(std::vector<std::vector<std::vector<int>>>& unsorted_vectors);
-
-	// heap sort 
-	void heapSort(std::vector<std::vector<int>>& unsorted_vectors, int heap_size);
-
-	// heapify
-	void heapify(std::vector<std::vector<int>>& unsorted_vectors, int num_vectors, int index);
-
-	// swap two vectors
-	void swap(std::vector<std::vector<int>>& unsorted_vectors, int min_child_index, int root_index);
-
-	// overloaded operator for vector comparison
-	bool compare(std::vector<int> left_vector, std::vector<int> rhs);
+	// calculate mixed base value of vector
+	int calc_mb_value(std::vector<int> vector);
 
 	// print a list of vectors grouped by Hamming norm
-	void print_sorted_vectors(std::vector<std::vector<std::vector<int>>> sorted_vectors);
+	void print_sorted_vectors(std::vector<std::map<int, std::vector<int>, std::greater<int>>> sorted_vectors);
 
 	// print a list of vectors
-	void print_vectors(std::vector<std::vector<int>>& all_vectors);
+	void print_vectors(std::map<int, std::vector<int>, std::greater<int>>& all_vectors);
 
 public:
 	// constructor
-	vector_generate(int* _kv_attributes, int num_attributes, bool print);
+	vector_generate(int* _kv_attributes, int _num_attributes, bool print);
 
 	// destructor
 	~vector_generate();
